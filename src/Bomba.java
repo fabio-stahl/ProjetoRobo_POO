be0c6b9ed455b1038485fb0f63173f5cb4f813a6
GlaucoCiprianoMoreira <glaucodx@gmail.com>
2025-05-16 15:35:49 -0300

    bomba bombeia so uma vez

--- src/Bomba.java
+++ src/Bomba.java
@@ -1,13 +1,19 @@
 import java.util.*;
 class Bomba extends Obstaculo {
+    private boolean ativada = true;
+    
     public Bomba(String id, int linha, int coluna) {
         super(id, linha, coluna);
     }
 
     @Override
     public void bater(Robo robo, List<Obstaculo> lista) {
+        if (!ativada) return;
+        
         System.out.println("ROBO " + robo.getCor() + " explodiu na bomba " + id + "!");
         robo.setExplodiu(true);
         lista.remove(this);
+
+        ativada = false;
     }
 }
